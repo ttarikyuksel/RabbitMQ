d2e811f76cf1b5995cfd4934e48ed7667cc5fee1
Tarik Yuksel <yukseltarik@hotmail.com>
2023-07-12 22:23:23 +0300

    RabbitMQ Hello World Project

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://gzszzate:tUeorBG__OUmMDs1yUSNNnSf7FpvJ90i@shark.rmq.cloudamqp.com/gzszzate");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "hello world";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "hello-queue",null,messageBody);
+
+Console.WriteLine("Mesaj gönderilmiştir");
+Console.ReadLine();
+
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://gzszzate:tUeorBG__OUmMDs1yUSNNnSf7FpvJ90i@shark.rmq.cloudamqp.com/gzszzate");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+// channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true, consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj: " + message);
+};
+
+Console.ReadLine();
